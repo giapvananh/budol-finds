3d2add48dfed764616d323444fd012e2fa18dbb5
VipAnh <vipanh130@gmail.com>
2026-05-12 09:37:47 +0800

    add vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700", "800", "900"],
